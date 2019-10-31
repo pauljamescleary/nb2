9726b80ee1efaddc55dde39cae1cfc57da170a4f
drunkirishcoder <daniel.jin@gmail.com>
2019-10-31 16:49:04 -0400

    add kni tx and rx pipelines

--- core/src/batch/rxtx.rs
+++ core/src/batch/rxtx.rs
@@ -2,11 +2,15 @@
 //!
 //! Implemented for `PortQueue`.
 //!
+//! `PacketRx` implemented for `KniRx`.
+//!
+//! `PacketTx` implemented for `KniTxQueue`.
+//!
 //! Implemented for `Vec` so it can be used as the batch source mostly
 //! in tests.
 
 use super::{PacketRx, PacketTx};
-use crate::{Mbuf, PortQueue};
+use crate::{KniRx, KniTxQueue, Mbuf, PortQueue};
 
 impl PacketRx for PortQueue {
     fn receive(&mut self) -> Vec<Mbuf> {
@@ -17,6 +21,18 @@ impl PacketRx for PortQueue {
 impl PacketTx for PortQueue {
     fn transmit(&mut self, packets: Vec<Mbuf>) {
         PortQueue::transmit(self, packets)
+    }
+}
+
+impl PacketRx for KniRx {
+    fn receive(&mut self) -> Vec<Mbuf> {
+        KniRx::receive(self)
+    }
+}
+
+impl PacketTx for KniTxQueue {
+    fn transmit(&mut self, packets: Vec<Mbuf>) {
+        KniTxQueue::transmit(self, packets)
     }
 }
 
